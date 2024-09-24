5461482a8a54ea4141e2974598c1001e4b185005
Jesus Pardo <jesus.pc.bt@gmail.com>
2024-09-25 00:33:37 +0200

    Added eCommerce contract with Price operations, autogeneration plugin with OpenAPI contract, controller and SwaggerUI configuration

---
+++ src/main/java/com/jeparca/inditex/priceservice/infrastructure/adapter/in/PriceController.java
@@ -0,0 +1,10 @@
+package com.jeparca.inditex.priceservice.infrastructure.adapter.in;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeparca.inditex.priceservice.api.v1.PricesApi;
+
+@RestController
+public class PriceController implements PricesApi {
+
+}
